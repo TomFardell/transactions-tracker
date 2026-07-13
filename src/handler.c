a052f1a9ece809539cdbc56078509d8e9c8e450b
TomFardell <thomasfardell@outlook.com>
2026-07-13 17:40:51 +0100

    Add method to parse the instruction from the body of a POST request

--- src/handler.c
+++ src/handler.c
@@ -1,7 +1,38 @@
 #include "handler.h"
 
 #include <stdbool.h>
+#include <stdio.h>
+
+#include "base/definitions.h"
+#include "base/string.h"
+#include "constants.h"
+
+// Given a request body, parse the instruction name. Returns an empty string if no instruction found
+static String request_body_get_instruction(String request_body) {
+  U64 instruction_label_pos = string_find_first(request_body, instruction_label);
+  if (instruction_label_pos == U64NULL) {
+    return string_literal("");
+  }
+
+  U64 after_instruction_label_pos = instruction_label_pos + instruction_label.len;
+  if (after_instruction_label_pos > request_body.len || request_body.str[after_instruction_label_pos] != '=') {
+    return string_literal("");
+  }
+
+  String after_instruction_label =
+      string_init_substring(request_body, after_instruction_label_pos + 1, request_body.len);
+
+  U64 instruction_name_end = string_find_first(after_instruction_label, string_literal("&"));
+  if (instruction_name_end == U64NULL) {
+    return after_instruction_label;
+  }
+
+  return string_init_substring(after_instruction_label, 0, instruction_name_end);
+}
 
 bool handle_post_data(MappingLists mapping_lists, String request_body) {
+  String instruction = request_body_get_instruction(request_body);
+  printf("Instruction: %" Stringf "\n", stringf_args(instruction));
+
   return false;
 }
--- src/handler.h
+++ src/handler.h
@@ -4,11 +4,10 @@
 #include <stdbool.h>
 
 #include "base/string.h"
-#include "helpers.h"
 #include "parser.h"
 
 // Given a POST request, parse it and (if possible) make the requested updates to the data described by the passed
-// mapping lists. Return whether the request was valid and the updates were made
+// mapping lists. Returns whether the request was valid and the updates were made
 bool handle_post_data(MappingLists mapping_lists, String request_body);
 
 #endif  // HANDLER_H
