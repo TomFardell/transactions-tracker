ad81760595af26b5ad445bc0d6d6bd1eebb9b470
TomFardell <thomasfardell@outlook.com>
2026-07-23 07:58:30 +0100

    Add logic to determine which members are needed in the '*add_to' command

--- src/handler.c
+++ src/handler.c
@@ -34,13 +34,14 @@ bool handle_post_data(MappingLists mapping_lists, String request_body) {
   Arena a = arena_init(2048);
 
   String instruction_string = request_body_get_instruction(request_body);
-  LinkNode *instruction_list = string_split(&a, instruction_string, string_literal("-"));
-  String instruction_name = linked_list_get_container_node_at_index(instruction_list, 0, StringNode, node)->data;
-  LinkNode *instruction_arguments = instruction_list->next;
+  LinkNode *instruction_arguments = string_split(&a, instruction_string, string_literal("-"));
+  String instruction_name =
+      linked_list_get_container_node_at_index(instruction_arguments, 0, StringNode, node)->data;
+  linked_list_remove_at_index(instruction_arguments, 0);
 
   if (string_equals(instruction_name, string_literal("*add_to"))) {
     if (linked_list_get_length(instruction_arguments) != 1) {
-      printf("Got '%" Stringf "' instruction with %" U64f "arguments\n", stringf_args(instruction_name),
+      printf("Got '%" Stringf "' instruction with %" U64f " arguments\n", stringf_args(instruction_name),
              linked_list_get_length(instruction_arguments));
       goto return_false;
     }
@@ -51,6 +52,71 @@ bool handle_post_data(MappingLists mapping_lists, String request_body) {
       printf("Couldn't locate list mapping '%" Stringf "'\n", stringf_args(list_name));
       goto return_false;
     }
+
+    typedef struct PathInfo {
+      U64 offset;
+      String id_path;      // E.g. this.member1.member2
+      String struct_path;  // E.g. type.member1.member2
+      DisplayType display_type;
+      InternalType internal_type;
+    } PathInfo;
+
+    typedef struct PathInfoNode {
+      PathInfo data;
+      LinkNode node;
+    } PathInfoNode;
+
+    LinkNode member_paths;
+    linked_list_init(&member_paths);
+    PathInfoNode initial_path = {.data = {.offset = 0,
+                                          .id_path = string_literal("this"),
+                                          .struct_path = list_mapping.item_struct_name,
+                                          .display_type = list_mapping.item_display_type,
+                                          .internal_type = list_mapping.item_internal_type}};
+    linked_list_push_back(&member_paths, &initial_path.node);
+
+    for (LinkNode *this_path = member_paths.next; this_path != &member_paths; /* Iteration inside body */) {
+      PathInfo this_path_info = link_node_get_container_node(this_path, PathInfoNode, node)->data;
+
+      LinkNode *member_mappings_for_path =
+          mapping_lists_get_member_mappings_for_struct(&a, mapping_lists, this_path_info.struct_path);
+
+      // If we found member mappings then this struct has members. We should remove this struct from our list
+      // and add the members instead
+      if (linked_list_get_length(member_mappings_for_path) > 0) {
+        // Add the members first
+        for (LinkNode *this_member = member_mappings_for_path->next; this_member != member_mappings_for_path;
+             this_member = this_member->next) {
+          MemberMapping this_member_mapping =
+              link_node_get_container_node(this_member, MemberMappingNode, node)->data;
+          PathInfoNode *this_path_info_node = arena_alloc_single(&a, PathInfoNode);
+          this_path_info_node->data = (PathInfo){
+              .offset = this_member_mapping.offset,
+              .id_path =
+                  string_concat(&a, 3, this_path_info.id_path, string_literal("."), this_member_mapping.name),
+              .struct_path =
+                  string_concat(&a, 3, this_path_info.struct_path, string_literal("."), this_member_mapping.name),
+              .display_type = this_member_mapping.display_type,
+              .internal_type = this_member_mapping.internal_type};
+
+          linked_list_push_back(&member_paths, &this_path_info_node->node);
+        }
+
+        this_path = this_path->next;
+        link_node_remove_from_linked_list(this_path->prev);
+      } else {
+        // No members so all there is to do is move the pointer forwards
+        this_path = this_path->next;
+      }
+    }
+
+    for (LinkNode *this_path = member_paths.next; this_path != &member_paths; this_path = this_path->next) {
+      PathInfo this_path_info = link_node_get_container_node(this_path, PathInfoNode, node)->data;
+      printf("-> %" Stringf "\n", stringf_args(this_path_info.id_path));
+    }
+
+    goto return_false;
+
   } else {
     printf("Got unrecognised instruction '%" Stringf "'\n", stringf_args(instruction_name));
     goto return_false;
